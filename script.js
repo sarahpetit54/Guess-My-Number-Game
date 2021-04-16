c58123a0428bb517695793ccc6958fd02f60f000
sarahpetit54 <79552516+sarahpetit54@users.noreply.github.com>
2021-04-16 17:06:58 -0500

    Added HTML & CSS Files

---
+++ script.js
@@ -0,0 +1,5 @@
+'use strict';
+document.querySelector(".message").textContent = `🎉 Correct Number!`
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 23;
