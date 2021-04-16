5db20e1a46e7e5852fcf8f4766a5c3fe119250e8
sarahpetit54 <79552516+sarahpetit54@users.noreply.github.com>
2021-04-16 17:48:50 -0500

    Started working on JS

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 'use strict';
-document.querySelector(".message").textContent = `🎉 Correct Number!`
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-document.querySelector('.guess').value = 23;
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = number;
+
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = `⛔ No Number!`;
+  } else if (guess === number) {
+    document.querySelector('.message').textContent = `🎉 Correct Number!`;
+  } else if (guess > number) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = `Too High!`;
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = `💥 You Lost The Game!`;
+      document.querySelector('.score').textContent = 0;
+    }
+  } else {
+    if (score > 1) {
+      document.querySelector('.message').textContent = `Too low!`;
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = `💥 You Lost The Game!`;
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
