22e33df68b6c11bb6727692bdca7859132749a07
sarahpetit54 <79552516+sarahpetit54@users.noreply.github.com>
2021-04-17 16:52:14 -0500

    finished JS

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 'use strict';
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = number;
-
+let number = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -10,6 +9,13 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = `⛔ No Number!`;
   } else if (guess === number) {
     document.querySelector('.message').textContent = `🎉 Correct Number!`;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = number;
+    if (score > highscore){
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   } else if (guess > number) {
     if (score > 1) {
       document.querySelector('.message').textContent = `Too High!`;
@@ -30,3 +36,15 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 });
+
+document.querySelector('.again').addEventListener("click", function() {
+  score = 20;
+  number = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = `Start guessing...`;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = "";
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  
+})
